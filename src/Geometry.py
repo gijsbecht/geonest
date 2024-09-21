22a287df09ab4beb978776011aff463f34311320
Gijs Becht <g.becht@brink.nl>
2024-09-21 14:29:49 +0200

    Refactor Geometry class to add reprojection capability

--- src/Geometry.py
+++ src/Geometry.py
@@ -1,5 +1,7 @@
 import shapely
 import geopandas as gpd
+import shapely.ops
+import pyproj
 
 class Geometry:
     def __init__(self, geom_wkt):
@@ -18,6 +20,17 @@ class Geometry:
             return self
         
         self.geom_shapely = shapely.buffer(self.geom_shapely, buffer_distance)
+        self.set_properties()
+        
+        return self
+    
+    def reproject(self, from_crs: str='EPSG:28992', to_crs: str='EPSG:4326'):
+        if not isinstance(self.geom_shapely, shapely.geometry.Polygon):
+            raise TypeError("Geometry must be a Polygon to reproject.")
+        
+        transformer = pyproj.Transformer.from_crs(from_crs, to_crs, always_xy=True)
+        transformed_coords = [transformer.transform(x, y) for x, y in self.geom_shapely.exterior.coords]
+        self.geom_shapely = shapely.geometry.Polygon(transformed_coords)
         self.set_properties()
         
         return self
