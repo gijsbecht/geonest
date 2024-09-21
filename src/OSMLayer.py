f4aeace30d86a9248e00279bc59a3d1d0ee82d5d
Gijs Becht <g.becht@brink.nl>
2024-09-21 14:30:11 +0200

    add get_geometries method

--- src/OSMLayer.py
+++ src/OSMLayer.py
@@ -1,5 +1,18 @@
-from  .Layer import Layer
+import osmnx as ox
+import geopandas as gpd
+from .Layer import Layer
+from .Geometry import Geometry  
 
 class OSMLayer(Layer):
-    def __init__(self, postgres_url: str) -> None:
+    def __init__(self, postgres_url: str, tags: dict, area: Geometry) -> None:
         super().__init__(postgres_url)
+
+        self.tags = tags
+        self.area = area
+        self.get_geometries()
+
+    def get_geometries(self):
+        try:
+            self.gdf = ox.features_from_polygon(self.area.reproject().geom_shapely, tags=self.tags)
+        except:
+            self.gdf  = gpd.GeoDataFrame()
