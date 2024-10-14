85252e3552efc339d4d2e5498a582fe6840c497c
Gijs Becht <g.becht@brink.nl>
2024-10-14 09:14:11 +0200

    updates

--- src/sql/sql.sql
+++ src/sql/sql.sql
@@ -32,21 +32,43 @@ DROP TABLE IF EXISTS geonest.buurten_result;
 
 CREATE TABLE IF NOT EXISTS geonest.buurten_result AS
 WITH parents_isochrone_30 AS (
-	SELECT * FROM geonest.the_parents_isochrones WHERE contour = 30
+	SELECT
+		buurten.buurtcode,
+		SUM(ROUND((ST_Area(ST_Intersection(the_parents_isochrones.geometry, buurten.geometry))/ST_Area(buurten.geometry))::numeric, 2)) AS overlap_parents_30
+	FROM 
+		geonest.buurten
+	JOIN geonest.the_parents_isochrones
+	ON ST_Intersects(the_parents_isochrones.geometry, buurten.geometry)
+	WHERE contour = 30
+	GROUP BY buurten.buurtcode
+),
+climbing_isochrone_15 AS (
+	SELECT
+		buurten.buurtcode,
+		SUM(ROUND((ST_Area(ST_Intersection(osm_climbing_isochrone.geometry, buurten.geometry))/ST_Area(buurten.geometry))::numeric, 2)) AS overlap_climbing_15
+	FROM 
+		geonest.buurten
+	JOIN geonest.osm_climbing_isochrone
+	ON ST_Intersects(osm_climbing_isochrone.geometry, buurten.geometry)
+	WHERE contour = 15
+	GROUP BY buurten.buurtcode
 )
-SELECT
+SELECT DISTINCT
 	buurten.buurtnaam,
 	buurten.gemeentenaam,
-	COALESCE(
-		SUM(ROUND((ST_Area(ST_Intersection(parents_isochrone_30.geometry, buurten.geometry))/ST_Area(buurten.geometry))::numeric, 2)),
-		0
-	) AS overlap_parents_30,
+	COALESCE(parents_isochrone_30.overlap_parents_30, 0) overlap_parents_30,
+	COALESCE(climbing_isochrone_15.overlap_climbing_15, 0) overlap_climbing_15,
+	COALESCE(parents_isochrone_30.overlap_parents_30, 0)*10+LEAST(COALESCE(climbing_isochrone_15.overlap_climbing_15, 0),2)*5 AS score,
 	buurten.geometry
 FROM
 	geonest.buurten
 JOIN geonest.the_parents_isochrones_45_intersection parents_isochrone_45
 ON ST_Intersects(buurten.geometry, parents_isochrone_45.geometry)
+JOIN geonest.osm_climbing_isochrone
+ON ST_Intersects(buurten.geometry, osm_climbing_isochrone.geometry)
 LEFT JOIN parents_isochrone_30
-ON ST_Intersects(buurten.geometry, parents_isochrone_30.geometry)
-WHERE buurten."stedelijkheidAdressenPerKm2" > 2
-GROUP BY buurten.buurtnaam, buurten.gemeentenaam, buurten.geometry
+ON parents_isochrone_30.buurtcode = buurten.buurtcode
+LEFT JOIN climbing_isochrone_15
+ON climbing_isochrone_15.buurtcode = buurten.buurtcode
+WHERE osm_climbing_isochrone.contour = 30
+AND buurten."stedelijkheidAdressenPerKm2" > 2
