a34b139605a7dad6afc85f9708a2fdc351484e1b
Gijs Becht <g.becht@brink.nl>
2024-10-20 12:18:28 +0200

    bugfix crs

--- src/Geometry.py
+++ src/Geometry.py
@@ -7,13 +7,14 @@ class Geometry:
     def __init__(self, geom_wkt):
         self.geom_wkt = geom_wkt
         self.geom_shapely = shapely.from_wkt(self.geom_wkt)
+        self.crs = 'EPSG:28992'
         self.set_properties()
     
     def set_properties(self):
         self.bbox = self.geom_shapely.bounds
         self.bbox_str = ",".join(map(str, self.bbox))
         self.bbox_shapely = shapely.geometry.box(*self.bbox)
-        self.gdf = gpd.GeoDataFrame(geometry=[self.geom_shapely], crs='EPSG:28992')
+        self.gdf = gpd.GeoDataFrame(geometry=[self.geom_shapely], crs=self.crs)
     
     def add_buffer(self, buffer_distance: int):
         if buffer_distance == 0:
@@ -28,6 +29,7 @@ class Geometry:
         if not isinstance(self.geom_shapely, shapely.geometry.Polygon):
             raise TypeError("Geometry must be a Polygon to reproject.")
         
+        self.crs = 'EPSG:4326'
         transformer = pyproj.Transformer.from_crs(from_crs, to_crs, always_xy=True)
         transformed_coords = [transformer.transform(x, y) for x, y in self.geom_shapely.exterior.coords]
         self.geom_shapely = shapely.geometry.Polygon(transformed_coords)
